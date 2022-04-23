cfd0f03c8e1cb6a4a756e01a036ee32433b98a9c
ESMAT001 <esmat10.niazi@gmail.com>
2022-04-23 13:49:39 +0430

    a

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const { getPlaiceholder } = require("plaiceholder");
 const app = express();
 const port = process.env.PORT || 3001
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/', async (req, res) => {
+    const { src, type } = req.query;
+    if (!src) res.status(400).send('No source provided');
+    try {
+        const image = await getPlaiceholder(src);
+
+        if (type) {
+            res.send({
+                img: image['img'],
+                [type]: image[type]
+            });
+        } else {
+            res.send(image);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 app.listen(port, () => {
